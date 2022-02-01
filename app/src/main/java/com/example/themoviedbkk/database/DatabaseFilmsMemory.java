cad88e2279dad1cf8279a2c3d0aaa68f03df133b
Krzysztof Krzyżaniak <krzyzaniak.krzysztof@o2.pl>
2022-02-01 13:18:11 +0100

    [KK] added database

---
+++ app/src/main/java/com/example/themoviedbkk/database/DatabaseFilmsMemory.java
@@ -0,0 +1,89 @@
+package com.example.themoviedbkk.database;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseFilmsMemory {
+
+    private final static String DATABASE_NAME = DatabaseParameters.DATABASE_NAME;
+    private final static int DATABASE_VERSION = DatabaseParameters.DATABASE_VERSION;
+
+    private final static String TABLE_FILMS = DatabaseParameters.TABLE_FILMY;
+    private final static String INDEX_UNIQUE_TABLE_FILMY_ON_ID_FILM_COLUMN = DatabaseParameters.KEY_UNIQUE_INDEX_TABLE_FILMY_ON_ID_FILM_COLUMN;
+
+    public  final static String KEY_ROWID = DatabaseParameters.ID_ROWID;
+    public  final static String KEY_ID_FILM = DatabaseParameters.ID_FILM_COLUMN;
+    public  final static String KEY_LIKE = DatabaseParameters.LIKE_COLUMN;
+
+
+    private final Context mCtx;
+    private DevicesHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+
+    private final String CREATE_TABLE_FILMS =
+            "CREATE TABLE if not exists " + TABLE_FILMS + " (" +
+                    KEY_ROWID + " integer PRIMARY KEY autoincrement ," +
+                    KEY_ID_FILM +  " integer default 0," +
+                    KEY_LIKE  + " integer default 0 " + ");";
+
+    private final String CREATE_INDEX_OF_TABLE_FILMS =
+            "CREATE UNIQUE INDEX " + INDEX_UNIQUE_TABLE_FILMY_ON_ID_FILM_COLUMN + " ON " + TABLE_FILMS + " (" + KEY_ID_FILM + ");";
+
+    public DatabaseFilmsMemory(Context ctx ) {
+        this.mCtx = ctx;
+
+    }
+
+    public DatabaseFilmsMemory open() throws SQLException {
+        mDbHelper = new DevicesHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        if (mDbHelper != null) {
+            mDbHelper.close();
+        }
+
+    }
+
+    public boolean isOpen() {
+
+        return mDb.isOpen();
+    }
+
+    private class DevicesHelper extends SQLiteOpenHelper {
+
+        DevicesHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(CREATE_TABLE_FILMS);
+            db.execSQL(CREATE_INDEX_OF_TABLE_FILMS);
+
+            Log.i("bazasql", "koniec create bazy");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+            Log.i("onupgrade", String.valueOf(oldVersion) + " --- " + String.valueOf(newVersion));
+
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_FILMS);
+
+            Log.i("bazasql", "koniec update");
+
+            onCreate(db);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/themoviedbkk/database/DatabaseParameters.java
@@ -0,0 +1,15 @@
+package com.example.themoviedbkk.database;
+
+public class DatabaseParameters {
+
+        public final static String DATABASE_NAME = "Filmy";
+        public final static int DATABASE_VERSION = 1;
+
+        public final static String TABLE_FILMY = "FilmyCodes";
+        public final static String KEY_UNIQUE_INDEX_TABLE_FILMY_ON_ID_FILM_COLUMN = "indexidfilm";
+
+        public static final String ID_ROWID = "Id";
+        public static final String ID_FILM_COLUMN = "IdFilm";
+        public static final String LIKE_COLUMN = "LikeFilm";
+
+}
